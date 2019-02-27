59a199c10129e1921ac528152609fbeef37764a3
unknown <ulyana250188@gmail.com>
2019-02-28 01:06:11 +0200

    Updates

---
+++ precache-manifest.491034a3ce0738babd99069c1c8d4bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "168cb224ec1fb4af1513",
+    "url": "/todo_react/static/js/runtime~main.168cb224.js"
+  },
+  {
+    "revision": "a6e2e5f638f3600c0e9c",
+    "url": "/todo_react/static/js/main.a6e2e5f6.chunk.js"
+  },
+  {
+    "revision": "fd523c38512c5d03ec57",
+    "url": "/todo_react/static/js/2.fd523c38.chunk.js"
+  },
+  {
+    "revision": "a6e2e5f638f3600c0e9c",
+    "url": "/todo_react/static/css/main.5e930853.chunk.css"
+  },
+  {
+    "revision": "f640c9024cad715bff8167c61e782b2b",
+    "url": "/todo_react/index.html"
+  }
+];
