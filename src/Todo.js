a5796c57df1ab6089e41cbae56297ffa18264056
unknown <ulyana250188@gmail.com>
2019-03-07 19:04:30 +0200

    made a little style

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 export default(props)=>(
 
-<div style={{display:"flex", justifyContent:"center"}}>
+<div style={{display:"flex", justifyContent:"center",paddingTop:20}}>
 
 
-<div style={{ textDecoration: props.todo.complite ? "line-through":""}} 
+<div style={{ textDecoration: props.todo.complite ? "line-through":"", paddingRight:60, fontSize:20,}} 
 onClick={props.selectTab}>
     {props.todo.text}
 </div>
-<button onClick={props.deleteTodo}>x</button>
+<button onClick={props.deleteTodo} style={{backgroundColor:'white', borderWidth: 0, color:'red' }}>x</button>
 </div>
 )
